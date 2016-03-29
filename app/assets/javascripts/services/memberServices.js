7d37b80406b7bdbba230bb534e54b70a4b6f773d
Tori Brenneison <tori.brenneison@gmail.com>
2016-03-28 21:12:54 -0400

    deleted stuff

--- app/assets/javascripts/services/memberServices.js
+++
@@ -1,9 +0,0 @@
-var coleadr = angular.module("coleadrApp"); 
-
-coleadr
-	.factory("getMembers", function($resource) { 
-	//get the members with t3h organization ID
-	var members = $resource("/organizations/:id/members");
-	return members; 
-	});
-	
