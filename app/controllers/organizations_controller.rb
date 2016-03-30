019be55eb16316c51c5c7e662a558ab95fb9432f
Tori Brenneison <tori.brenneison@gmail.com>
2016-03-29 21:44:18 -0400

    make organizationsgit add . git add .  wowzers

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,4 +10,32 @@ class OrganizationsController < ApplicationController
     render json: organization
   end 
   
+  def new 
+    #GET
+    #generates model object that will be passed to the view and posted to create action
+    @organization = Organization.new 
+  end
+  
+    #POST
+    #controller gets nested hash params[:organization] with the attributes set in the form
+    def create
+      organization = Organization.new(organization_request_params)
+      if organization.save
+        render json: organization
+      else
+        #return http 422 unprocessable entity
+        render json: {errors: organization.errors.full_messages}, status: 422
+      end
+    end
+
+  
+  private 
+    def organization_request_params
+      #security measure - these are the only parameters that will be accepted 
+      #hands back a filtered hash of values to be handed back in to the action 
+        params.require(:organization).permit(
+          :name
+        )
+      end 
+  
 end
