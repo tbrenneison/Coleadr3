498a0cae8333987d400150f8b273756a1e7792af
Tori Brenneison <tori.brenneison@gmail.com>
2016-05-14 16:48:22 -0400

    marketing site navbar and CTA

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 // 
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap
+//= require jquery_ujs
 //= require angular
 //= require angular-route
 //= require angular-resource
