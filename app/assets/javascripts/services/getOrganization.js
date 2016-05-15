b9d72801b55b0d5feb8259cf6a39280f98bd034f
Tori Brenneison <tori.brenneison@gmail.com>
2016-05-15 12:01:00 -0400

    finished out organization details page and still futzing with the css a bit

--- app/assets/javascripts/services/getOrganization.js
+++ app/assets/javascripts/services/getOrganization.js
@@ -2,12 +2,14 @@ var coleadr = angular.module("coleadrApp");
 
 coleadr.factory("getOrganization", function($http) { 
 	//get the current organization
-	var organization = {name:"", id:""}; 
+	var organization = {name:"", id:"", created_at:"", last_update:""}; 
 	$http.get("../organizations/json")
     .then(function(response) {
 		 console.log(response.data);
 		 organization.id = response.data.id; 
 		 organization.name = response.data.name;
+		 organization.created_at = response.data.created_at; 
+		 organization.last_update = response.data.updated_at;
 	   });
 	return organization; 
 });
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   
-  get "/:page" => "home#show"
-
-  
   resources :accounts, only: [:new, :create]
   
   resources :organizations, except: [:edit], shallow: true do
